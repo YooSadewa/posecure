71c08ec24fb982c573b998845ee761f7622c33fc
ftzahra <fatiahfatiah39@gmail.com>
2025-12-18 11:20:00 +0700

    Fixing bugs on the Citizen Account List page

--- petugas_page/process/hapus_akun_warga_proses.php
+++ petugas_page/process/hapus_akun_warga_proses.php
@@ -14,8 +14,9 @@ if (!isset($_POST['id_user'])) {
 
 $id_user = $_POST['id_user'];
 
+mysqli_query($conn, "DELETE FROM absensi WHERE id_user = '$id_user'");
+mysqli_query($conn, "DELETE FROM insiden_keamanan WHERE id_user = '$id_user'");
 mysqli_query($conn, "DELETE FROM warga WHERE id_user = '$id_user'");
-
 mysqli_query($conn, "DELETE FROM user WHERE id_user = '$id_user'");
 
 $_SESSION['alert'] = [
